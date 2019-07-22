584d9684b0232cc1834a787ac395c01ee54b5af5
Robosensus1 <53072093+Robosensus1@users.noreply.github.com>
2019-07-22 17:25:39 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ sh ./generate_credentials.sh > credentials.yml
 
 
 echo "++++++++++ STARTING SERVER ++++++++++"
-python -m rasa_core.run -d models/dialogue -u models/current/nlu  --port $PORT --credentials credentials.yml
+python rasa run
